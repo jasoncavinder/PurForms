1076efb444f642d805f9eb978a6faba2068eaa8e
Jason Cavinder <jason.cavinder@gmail.com>
2013-08-09 21:55:48 -1000

    Initial Commit

---
+++ PurForms/PF_Document.h
@@ -0,0 +1,13 @@
+//
+//  PF_Document.h
+//  PurForms
+//
+//  Created by Jason Cavinder on 8/9/13.
+//  Copyright (c) 2013 Jason Cavinder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PF_Document : NSDocument
+
+@end
---
+++ PurForms/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PurForms
+//
+//  Created by Jason Cavinder on 8/9/13.
+//  Copyright (c) 2013 Jason Cavinder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
